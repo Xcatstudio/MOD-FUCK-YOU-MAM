1c9df626cca922cce9764b220ff4c0c5cfe500ca
Xcat Studio <vlad120rada@gmail.com>
2026-06-07 11:41:00 +0200

    Fix: use UILayer approach to find and lock music arrow

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,42 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/CCSprite.hpp>
+#include <Geode/modify/UILayer.hpp>
 
 using namespace geode::prelude;
 
-class $modify(CCSprite) {
-    void setRotation(float rotation) override {
-        if (Mod::get()->getSettingValue<bool>("enabled")) {
-            auto* frame = this->getSpriteFrame();
-            if (frame) {
-                auto name = std::string(frame->getName());
-                if (name.find("musicArrow") != std::string::npos ||
-                    name.find("musicBtn") != std::string::npos)
-                {
-                    CCSprite::setRotation(Mod::get()->getSettingValue<double>("angle"));
-                    return;
-                }
+static CCNode* g_musicArrow = nullptr;
+
+void findMusicArrow(CCNode* node) {
+    if (g_musicArrow) return;
+    if (auto sprite = typeinfo_cast<CCSprite*>(node)) {
+        if (auto frame = sprite->getSpriteFrame()) {
+            auto name = std::string(frame->getName());
+            if (name.find("musicArrow") != std::string::npos ||
+                name.find("musicBtn") != std::string::npos)
+            {
+                g_musicArrow = sprite;
+                return;
             }
         }
-        CCSprite::setRotation(rotation);
+    }
+    for (int i = 0; i < node->getChildrenCount(); i++) {
+        if (auto child = typeinfo_cast<CCNode*>(node->getChildren()->objectAtIndex(i))) {
+            findMusicArrow(child);
+        }
+    }
+}
+
+class $modify(UILayer) {
+    bool init(GJBaseGameLayer* layer) {
+        if (!UILayer::init(layer)) return false;
+        g_musicArrow = nullptr;
+        findMusicArrow(this);
+        return true;
+    }
+
+    void update(float dt) override {
+        UILayer::update(dt);
+        if (g_musicArrow && Mod::get()->getSettingValue<bool>("enabled")) {
+            g_musicArrow->setRotation(Mod::get()->getSettingValue<double>("angle"));
+        }
     }
 };
